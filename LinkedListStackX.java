9a09bde53b16c64a451824487857f1a9d231da55
Abiola David <davidolanrewaju2000@gmail.com>
2021-09-10 15:56:25 +0100

    Implemented a stack using a LinkedList

---
+++ LinkedListStackX.java
@@ -0,0 +1,44 @@
+package com.sirdave.java_ds_book;
+
+public class LinkedListStackX {
+    DoubleEndedList list;
+
+    public LinkedListStackX(){
+        list = new DoubleEndedList();
+    }
+
+    public void push(double value){
+        list.insertFirst(value);
+    }
+
+    public double pop(){
+        Link data =  list.deleteFirst();
+        return data.dData;
+    }
+
+    public boolean isEmpty(){
+        return list.isEmpty();
+    }
+
+    public void displayStack(){
+        System.out.println("Stack from top to bottom");
+        list.display();
+    }
+
+    public static void main(String[] args){
+        LinkedListStackX stack = new LinkedListStackX();
+        stack.push(12);
+        stack.push(28);
+        stack.push(2);
+        stack.push(4);
+        stack.push(7);
+        stack.push(19);
+
+        stack.displayStack();
+
+        while (!stack.isEmpty()){
+            double value = stack.pop();
+            System.out.println("value is " + value);
+        }
+    }
+}
