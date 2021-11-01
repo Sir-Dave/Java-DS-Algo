4f1f426f24d8ab88f97d2adf733d63af9ac7a39d
Abiola David <davidolanrewaju2000@gmail.com>
2021-11-01 08:25:07 +0100

    Remove duplicates in a LinkedList

--- LinkedListSolution.java
+++ LinkedListSolution.java
@@ -1,8 +1,6 @@
 package com.sirdave.data_structures;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 class ListNode {
     int val;
@@ -16,7 +14,7 @@ class ListNode {
 }
 
 
-public class LinkedListMerger {
+public class LinkedListSolution {
     static ListNode head;
 
     public static ListNode createLinkedList(ArrayList<Integer> arr) {
@@ -58,13 +56,33 @@ public class LinkedListMerger {
         return dummy.next;
     }
 
+    static ListNode removeDuplicates(ListNode head){
+        Set<Integer> set = new HashSet<>();
+        ListNode current = head;
+        ListNode prev = null;
+        while (current != null){
+            if (set.contains(current.val)){
+                prev.next = current.next;
+                current = prev.next;
+            }
+            else {
+                set.add(current.val);
+                prev = current;
+                current = current.next;
+            }
+        }
+        return head;
+    }
+
     public static void main(String[] args) {
         ArrayList<Integer> a = new ArrayList<>(List.of(1,2,4));
-        ArrayList<Integer> b = new ArrayList<>(List.of(1,3,4));
+        ArrayList<Integer> b = new ArrayList<>(List.of(1,3,4, 7, 8, 9));
         ListNode l1 = createLinkedList(a);
         ListNode l2 = createLinkedList(b);
-
-        ListNode current = mergeTwoLists(l1, l2);
+        //ListNode current = mergeTwoLists(l1, l2);
+        ArrayList<Integer> c = new ArrayList<>(List.of(1,2,2, 3, 4, 4, 5));
+        ListNode head = createLinkedList(c);
+        ListNode current = removeDuplicates(head);
         while (current != null){
             System.out.println(current.val);
             current = current.next;
